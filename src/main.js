20a5cae29e9b3b5e8118aa7d1e7cb85cd76df144
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-04-30 15:35:21 +0200

    added bulma and navbar

--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
-import ViewNotes from "@/views/ViewNotes.vue";
+import router from '@/router/index'
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "notes",
-      component: ViewNotes,
-    },
-  ],
-});
+
+
 
 createApp(App).use(router).mount("#app");
